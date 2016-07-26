93125ddead33f615c31d293abaf2ab50d5083575
mortonprod <mortonprod@gmail.com>
2016-07-26 18:28:16 +0100

    Add smaller helper class to get directory structure. Add configurations to build website for dev/production etc...

---
+++ webpackConfig/util/webpack.dev.ts
@@ -0,0 +1,30 @@
+﻿///TODO:Why do I not need to define reference in this file. Breaks if I take it out of webpack.common.ts???
+var webpackMerge = require('webpack-merge');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var commonConfig = require('./webpack.common.js');
+var helpers = require('./helpers');
+
+console.log("Start webpack development.");
+
+module.exports = webpackMerge(commonConfig, {
+    node: {
+        fs: "empty"
+    },
+    devtool: 'cheap-module-eval-source-map',
+
+    output: {
+        path: helpers.root('dist'),
+        publicPath: 'http://localhost:8080/',
+        filename: '[name].js',
+        chunkFilename: '[id].chunk.js'
+    },
+
+    plugins: [
+        new ExtractTextPlugin('[name].css')
+    ],
+
+    devServer: {
+        historyApiFallback: true,
+        stats: 'minimal'
+    }
+});
