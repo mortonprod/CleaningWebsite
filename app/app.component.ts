b306813b5dd302f47eb7ed75ab0a23ff8b2a07f1
mortonprod <mortonprod@gmail.com>
2016-07-28 15:57:35 +0100

    Add example angular start ts files.

---
+++ app/app.component.ts
@@ -0,0 +1,29 @@
+﻿import { Component, ViewEncapsulation } from '@angular/core';
+import { AppState } from './app.service';
+
+@Component({
+    selector: 'app',
+    encapsulation: ViewEncapsulation.None,
+    styleUrls: [
+        './app.style.css'
+    ],
+    template: `
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+  `
+})
+export class App {
+    cleaningclassLogo = 'assets/img/angularclass-avatar.png';
+    name = 'Cleaning Website';
+    url = 'https://facebook.com/cleaning';
+
+    constructor(
+        public appState: AppState) {
+
+    }
+    ngOnInit() {
+        console.log('Initial App State', this.appState.state);
+    }
+
+}
