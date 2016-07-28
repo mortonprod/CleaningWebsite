35a5506fd272c1a03c26258bc830c266a0c3558f
mortonprod <mortonprod@gmail.com>
2016-07-28 16:29:36 +0100

    Add sqr component ts files

---
+++ app/cleanFloor/sqr/sqr.component.ts
@@ -0,0 +1,35 @@
+﻿import { Component } from '@angular/core';
+import { AppState } from '../app.service';
+
+@Component({
+    selector: 'cleanFloor',  // <home></home>
+    providers: [
+    ],
+    directives: [
+    ],
+    pipes: [],
+    styleUrls: ['./cleanFloor.style.css'],
+    templateUrl: './cleanFloor.template.html'
+})
+
+export class cleanFloor {
+    // Set our default values
+    localState = { value: '' };
+    // TypeScript public modifiers
+    constructor(public appState: AppState) {
+
+    }
+
+    ngOnInit() {
+        console.log('hello `cleanFloor` component');
+        // this.title.getData().subscribe(data => this.data = data);
+    }
+
+    submitState(value) {
+        console.log('submitState', value);
+        this.appState.set('value', value);
+        this.localState.value = '';
+    }
+
+}
+
