e9002a836a693c237c26cadb45430439e660281b
mortonprod <mortonprod@gmail.com>
2016-08-01 01:49:59 +0100

    Add booking component
    This component should be composed of date and time component.
    At the moment is simply uses the booking service to query the server

---
+++ app/mainPage/booking/booking.component.ts
@@ -0,0 +1,21 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { jobsService,job } from '../../app.service';
+@Component({
+    selector: 'booking',
+    providers: [jobsService],
+    pipes: [],
+    templateUrl: './booking.template.html'
+})
+
+export class booking implements OnInit {
+    //TODO:Need to put private for this to work in getJobs? Why?
+    jobs = new Array<job>();
+    error = "";
+    constructor(private jbServ: jobsService) {
+    }
+    ngOnInit() {
+    }
+    getJobs() {
+        this.jbServ.getJobs().subscribe(jobs => this.jobs = jobs, error => this.error = <any>error);
+    }
+}
