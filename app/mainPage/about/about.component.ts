b71e542bc27cc3f2dd5e86ba6fe614cf7051408d
mortonprod <mortonprod@gmail.com>
2016-08-03 00:52:01 +0100

    Add about page
    Spliting the full site into individual pages to be served by angular2
    router. About page will act as intro.

---
+++ app/mainPage/about/about.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'about',
+    templateUrl: './about.template.html',
+    styleUrls: ['./about.style.css'],
+})
+
+export class about{
+    constructor() {
+
+    }
+
+}
