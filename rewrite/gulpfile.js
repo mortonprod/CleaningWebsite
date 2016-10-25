04cb3dc35359b5aeb5d9d3ac78a7f8d90b95ed57
mortonprod <mortonprod@gmail.com>
2016-10-25 21:54:41 +0100

    Create new gulp file
    This gulp file will only move the vendor code from src to dist.

---
+++ rewrite/gulpfile.js
@@ -0,0 +1,15 @@
+﻿var gulp = require('gulp');
+///var webpack = require('webpack-stream');
+///gulp.task('default', function () {
+///    return gulp.src('src/indexEntry.js')
+///        .pipe(webpack(require('./webpack.config.js')))
+///        .pipe(gulp.dest('dist/'));
+///});
+
+gulp.task('moveVendor', function () {
+    gulp.src([
+        "src/vendor/*"
+    ], { base: "src" })
+        .pipe(gulp.dest('dist'));
+});
+
