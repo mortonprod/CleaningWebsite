9c186e3147b5b31d5f494c9046bb01484cb0d558
mortonprod <mortonprod@gmail.com>
2016-10-26 22:02:09 +0100

    Create the start of a simple node server
    This is based on an old project. It will not work as expected at the
    moment.

---
+++ rewrite/src/ts/server.ts
@@ -0,0 +1,30 @@
+﻿var express = require('express');
+var path = require('path');
+var app = express();
+
+
+app.get('/contact', function (req, res) {
+
+    //TODO:Note:This is how we create a new user after setting up the db in models/user. The db will be created automatically.
+    var nick = new User({
+        name: 'Nick Cerminara',
+        password: 'password',
+        admin: true
+    });
+
+    // save the sample user
+    nick.save(function (err) {
+        if (err) {
+            res.json({ success: false });
+            throw err;
+        }
+
+        console.log('User saved successfully');
+        res.json({ success: true });
+    });
+});
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function () {
+});
