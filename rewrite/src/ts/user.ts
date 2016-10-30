ee3620eb01337064886f15872efac6b03cc5cf37
mortonprod <mortonprod@gmail.com>
2016-10-30 15:22:38 +0000

    Create user

---
+++ rewrite/src/ts/user.ts
@@ -0,0 +1,8 @@
+﻿///@function findOne
+///@params obj Passed username to check against.
+///@params cb This is the function which will redirect depending on success of login.
+///A function which will take username and password and look for user. 
+///Need to pass err and user to callback function for passport to call done on.
+function findUser(obj, cb){
+
+}
