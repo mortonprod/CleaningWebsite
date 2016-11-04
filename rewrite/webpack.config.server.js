0d7c1f050d7876f1f191a581951600944cb58a03
mortonprod <mortonprod@gmail.com>
2016-11-04 22:18:36 +0000

    Add server webpack config

---
+++ rewrite/webpack.config.server.js
@@ -0,0 +1,31 @@
+﻿'use strict';
+var webpack = require('webpack');
+var config = {
+    entry: {
+        server: ['./src/ts/server.ts']
+
+    },
+    target: "node",
+    node: {
+        fs: "empty"
+    },
+    plugins: [
+    ],
+
+    output: {
+        path: './dist',
+        filename: '[name].js',
+        libraryTarget: 'umd' // Need this for static site generation.
+    },
+    module: {
+        preLoaders: [
+            { test: /\.json$/, exclude: /node_modules/, loader: 'json' },
+        ],
+        loaders: [
+            { test: /\.json$/, loader: 'json' },
+            { test: /\.tsx?$/, loader: "ts-loader" }
+        ]
+    }
+};
+
+module.exports = config;
