d932600cac74dfb4acfe2c136f7a8c779d51858a
mortonprod <mortonprod@gmail.com>
2016-11-11 17:19:42 +0000

    Remove old files to place in the right place

--- app/mainPage/booking/cleanTimePicker/index.ts
+++
@@ -1,1 +0,0 @@
-﻿export * from './cleanTimePicker.component'
--- rewrite/src/client/reducers/index.ts
+++
@@ -1,9 +0,0 @@
-﻿import { combineReducers } from 'redux';
-import { customers } from "./LoginSignupReducer.ts";
-
-const RootReducer = combineReducers({
-    customers: customers
-
-});
-
-export default RootReducer;
