55282a32af4703fd432bb0449c90404158258586
mortonprod <mortonprod@gmail.com>
2016-11-11 17:09:54 +0000

    Add global variables and passport initialisation

---
+++ rewrite/src/server/config/global.ts
@@ -0,0 +1,23 @@
+﻿/// @function This is the module for storing all variables which might be used throughout the site.
+export default function global() {
+    var con = {
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'root',
+        database: 'cleanBase'
+    };
+    var mail = {
+        user: 'atticusfbf@gmail.com',
+        pass: '975975975'
+    };
+    var mailOptions = {
+        from: 'atticusfbf@gmail.com', // This does not do anything????
+        to: 'mortonprod@gmail.com' // list of receivers
+    };
+    return {
+        con: con,
+        mail: mail,
+        mailOptions: mailOptions
+    }
+}
