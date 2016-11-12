7b8a78ef900f016cd01714cc2bcecfe3789d29bf
mortonprod <mortonprod@gmail.com>
2016-11-12 22:01:51 +0000

    Update index
    Remove thunk to pass dispatch as argument
    Remove require ensure

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,5 +1,4 @@
-﻿/// <reference path="../../typings/index.d.ts" />
-import "jquery";
+﻿import "jquery";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import "./sass/site.scss";
@@ -10,23 +9,19 @@ import thunkMiddleware from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import RootReducer from './reducers/index.ts';
-
-const createStoreWithMiddleware = applyMiddleware(
-    thunkMiddleware // lets us dispatch() functions
-)(createStore);
-
-const store = createStoreWithMiddleware(RootReducer, window.__PRELOADED_STATE__);
-
-
-require.ensure(["./containers/contact.tsx"], function (require) {
-    var Contact = require("./containers/contact.tsx");
-    reactDOM.render(
-        <Provider store={store} >
-            <Contact/>
-        </Provider>,
-        document.getElementById("contact"))
-},"contact");
-
+import Contact from "./containers/contact.tsx";
+//const createStoreWithMiddleware = applyMiddleware(
+//    thunkMiddleware // lets us dispatch() functions
+//)(createStore);
+//
+//const store = createStoreWithMiddleware(RootReducer, window.__PRELOADED_STATE__);
+export const store = createStore(RootReducer, window.__PRELOADED_STATE__)
+reactDOM.render(
+    <Provider store={store} >
+        <Contact/>
+    </Provider>,
+    document.getElementById("contact")
+);
 
 
 
