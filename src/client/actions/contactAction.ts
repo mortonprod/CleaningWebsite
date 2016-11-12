bca277c143e3864c3fec03bfd2989a85b811874f
mortonprod <mortonprod@gmail.com>
2016-11-12 21:58:50 +0000

    Update contact action
    Fix typescript actions
    Move dispatch of action

--- src/client/actions/contactAction.ts
+++ src/client/actions/contactAction.ts
@@ -1,47 +1,35 @@
-﻿/// <reference path="../../../typings/index.d.ts" />
-import * as $ from "jquery";
-import * as Promise from "promises";
-
-export function sendMessage(name, email, message) {
-    return (dispatch) => {
+﻿import * as $ from "jquery";
+import Promise from "ts-promise";
+export function sendMessage(name: string, email: string, message: string) {
+    return (dispatch: any) => {
         dispatch(sendingMessage(true));
-        validateMessage(name, email, message).then( function (name, email, message) {
+        let helpMessage: Array<string> = validation(name, email, message);
+        ///validation code here
+        if (helpMessage === []) {
             dispatch(sendMessageToServer(name, email, message));
-        }, function (name, email, message, helpMessage) {
-            dispatch(sendToStoreWithHelpMessage(name, email, message, helpMessage));
-        })
-    }
-}
-
-function validateMessage(name, email, message) {
-    let helpMessage = validation(name,email,message);
-    ///Put validation code here.
-    return new Promise(function (resolve, reject) {
-        if (helpMessage ===[]) {
-            resolve(name, email, message);
         } else {
-            reject(name, email, message, helpMessage);
+            dispatch(sendToStoreWithHelpMessage(name, email, message, helpMessage));
         }
-    })
-    function validation(name,email,message) {
-        let helpMessage = [];
-        let help = null
-        ///Check if using the same name as store.
-        if (help !==null){
-            helpMessage.push(help)
+        function validation(name: string, email: string, message: string) {
+            let helpMessage: Array<string> = [];
+            let help: string = null
+            ///Check if using the same name as store.
+            if (help !== null) {
+                helpMessage.push(help)
+            }
+            help = null
+            ///Check email has an @ sign.
+            if (help !== null) {
+                helpMessage.push(help)
+            }
+            return helpMessage;
         }
-        help = null
-        ///Check email has an @ sign.
-        if (help !== null) {
-            helpMessage.push(help)
-        }
-        return helpMessage;
     }
 }
 
 /// @function action creator. This will send message.
-function sendMessageToServer(name, email, message) {
-    return (dispatch) => {
+function sendMessageToServer(name:string, email:string, message:string) {
+    return (dispatch:any) => {
         $.ajax({
             type: 'POST',
             url: '/contact',
@@ -49,7 +37,7 @@ function sendMessageToServer(name, email, message) {
         }).done(function (data) {
             if (data.error) {
                 console.log("add todo worked but error: ", data);
-                dispatch(sendToStoreWithHelpMessage(name, email, message, "Error sending to server"));
+                dispatch(sendToStoreWithHelpMessage(name, email, message, ["Error sending to server"]));
                 dispatch(sendingMessage(false));
             } else {
                 dispatch(sendToStoreWithHelpMessage(name, email, message));
@@ -57,7 +45,7 @@ function sendMessageToServer(name, email, message) {
             }
         }).fail(function (a, b, c, d) {
             console.log("actual failure: ", a, b, c, d)
-            dispatch(sendToStoreWithHelpMessage(name, email, message,"Error sending to server"));
+            dispatch(sendToStoreWithHelpMessage(name, email, message,["Error sending to server"]));
             dispatch(sendingMessage(false));
         });
     }
@@ -68,15 +56,15 @@ function sendMessageToServer(name, email, message) {
 export const SEND_MESSAGE = 'SEND_MESSAGE';
 export const STORE_MESSAGE = 'STORE_MESSAGE';
 export const SENDING_MESSAGE = 'SENDING_MESSAGE';
-function sendingMessage(bool) {
+function sendingMessage(bool: boolean) {
     return {
         type: SENDING_MESSAGE,
-        bool
+        sending:bool
     }
 }
 
 
-function sendToStoreWithHelpMessage(name,email,message,helpMessage="completed"){
+function sendToStoreWithHelpMessage(name:string,email:string,message:string,helpMessage=["completed"]){
     return {
         type: SEND_MESSAGE,
         name,
@@ -86,7 +74,7 @@ function sendToStoreWithHelpMessage(name,email,message,helpMessage="completed"){
     }
 }
 
-export function failMessage(name,email,message) {
+export function failMessage(name:string,email:string,message:string) {
     return {
         type: SENDING_MESSAGE,
         contactLoad: true
