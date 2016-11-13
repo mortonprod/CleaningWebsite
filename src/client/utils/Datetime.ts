beaed6185b9a155c6902d3e6cdd7ad24ac627c65
mortonprod <mortonprod@gmail.com>
2016-11-13 23:49:45 +0000

    Add datetime class to easily store dates

---
+++ src/client/utils/Datetime.ts
@@ -0,0 +1,15 @@
+﻿export default class DateTimes {
+    public day: number;
+    public month: number;
+    public year: number;
+    public minute: number;
+    public hour: number;
+
+    constructor(day: number, month: number, year: number, minute: number, hour: number) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        this.minute = minute;
+        this.hour = hour;
+    }
+}
