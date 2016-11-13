050b8e78a6a9d3eba644985ae805fd8679377b77
mortonprod <mortonprod@gmail.com>
2016-11-13 23:49:03 +0000

    Put all reducer in a single file
    A single reducer function for each set of actions

--- src/client/reducers/index.ts
+++ src/client/reducers/index.ts
@@ -1,9 +1,89 @@
 ﻿import { combineReducers } from 'redux';
-import { customers } from "./LoginSignupReducer";
-import { createStore } from 'redux';
-const RootReducer = combineReducers({
-    customers: customers
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import DateTimes from "../utils/datetime";
+import actionNames from "../actions/actionNames";
 
+
+let holder: Array<DateTimes> = [];
+let holder2: Array<string> = [];
+const initialState = {
+    user: {
+        name: "",
+        email: "",
+        dateTime:holder
+    },
+    contact: {
+        message: "",
+        helpMessage: holder2,
+        sendingMessage: false,
+    }
+}
+function userReducer(state = initialState.user, action: any) {
+    switch (action.type) {
+        case  actionNames().addName:
+            return Object.assign({}, state, {
+                user: {
+                    name: action.name,
+                    email: state.email,
+                    dateTime: state.dateTime
+                }
+            })
+        case actionNames().addEmail:
+            return Object.assign({}, state, {
+                user: {
+                    name: state.name,
+                    email: action.email,
+                    dateTime: state.dateTime
+                }
+            })
+        case actionNames().addUserDatesTimes:
+            return Object.assign({}, state, {
+                user: {
+                    name: state.name,
+                    email: state.email,
+                    dateTime: action.dateTime
+                }
+            })
+        default:
+            return state
+    }
+}
+
+function contactReducer(state = initialState.contact, action: any) {
+    switch (action.type) {
+        case actionNames().addMessage:
+            return Object.assign({}, state, {
+                contact: {
+                    message: action.message,
+                    helpMessage: state.helpMessage,
+                    sendingMessage: state.sendingMessage
+                }
+            })
+        case actionNames().addHelpMessage:
+            return Object.assign({}, state, {
+                contact: {
+                    message: state.message,
+                    helpMessage: action.message,
+                    sendingMessage: state.sendingMessage
+                }
+            })
+        case actionNames().addSendingMessage:
+            return Object.assign({}, state, {
+                contact: {
+                    message: state.message,
+                    helpMessage: state.message,
+                    sendingMessage: action.sendingMessage
+                }
+            })
+        default:
+            return state
+    }
+}
+
+const RootReducer = combineReducers({
+    userReducer,
+    contactReducer
 });
 ///If we are running on client then fill with preloaded state. Otherwise use initial state from server.
 var initial: any;
@@ -13,5 +93,8 @@ if (typeof window !== "undefined" && typeof window.__PRELOADED_STATE__ !== "unde
     initial = (function () { return; })();
     console.log("No window initial state specified. Set initial to undefined")
 }
-export default function store() { return createStore(RootReducer, initial) }
+const createStoreWithMiddleware = applyMiddleware(
+    thunkMiddleware // lets us dispatch() functions
+)(createStore);
 
+export default function store() { return createStoreWithMiddleware(RootReducer, initial); }
