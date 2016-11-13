9834733d31e96c9c16f86dc710d89ff69da2ac7e
mortonprod <mortonprod@gmail.com>
2016-11-13 23:46:27 +0000

    Add action names file
    Keep all action names in a single location

---
+++ src/client/actions/actionNames.ts
@@ -0,0 +1,11 @@
+﻿export default function actionNames() {
+    return {
+        addName:"ADD_NAME",
+        addEmail: "ADD_EMAIL",
+        addUserDatesTimes: "ADD_USER_DATES_TIMES",
+        addMessage: "ADD_MESSAGE",
+        addHelpMessage: "ADD_HELP_MESSAGE",
+        addSendingMessage:"ADD_SENDING_MESSAGE",
+        addAllDatesTimes: "ADD_ALL_DATES_TIMES",
+    }
+}
