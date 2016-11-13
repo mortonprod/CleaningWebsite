64188f05f30b86423141b523c325ce869d3dc12c
mortonprod <mortonprod@gmail.com>
2016-11-13 16:15:26 +0000

    Add gulpfile and client and server config
    Add files to build unbundled server and client javascript

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+﻿var gulp = require('gulp');
+var shell = require('gulp-shell')
+var ts = require("gulp-typescript");
+
+
+///@function default
+/// This will transpile the server code, move vendor code, then pug files
+gulp.task("default", ['client','server'], function () {
+ 
+});
+
+gulp.task('client', shell.task([
+    'webpack',
+]))
+
+
+//gulp.task('server', ['movePug','clientOnServer'], function () {
+//    return tsProjectServer.src()
+//        .pipe(tsProject())
+//        .js.pipe(gulp.dest("dist"));
+//
+//});
+
+gulp.task('clientOnServer', [], function () {
+    var tsProject = ts.createProject('./tsconfig-server.json');
+    var tsresult = tsProject.src()
+     //   .pipe(sourcemaps.init())
+        .pipe(tsProject());
+
+    return tsresult.js
+       // .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist')); 
+
+});
+
+gulp.task('movePug', function () {
+    gulp.src([
+        "src/server/pug/**"
+    ], { base: "src/server" })
+        .pipe(gulp.dest('dist/server'));
+});
+
