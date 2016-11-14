d3b8860ffa3e5a43943ec45fead604b2f689ce41
mortonprod <mortonprod@gmail.com>
2016-11-14 18:48:16 +0000

    Add smooth transition code

---
+++ src/client/utils/smoothTransition.ts
@@ -0,0 +1,19 @@
+﻿import * as $ from "jquery";
+import "smoothstate";
+var $body = $('html, body'); 
+var content = $('#main').smoothState({
+    // onStart runs as soon as link has been activated
+    onStart: {
+
+        // Set the duration of our animation
+        duration: 250,
+
+        // Alterations to the page
+        render: function () {
+
+            // Quickly toggles a class and restarts css animations
+            content.toggleAnimationClass('is-exiting');
+            $body.animate({ 'scrollTop': 0 });
+        }
+    }
+}).data('smoothState'); // makes public methods available
