ce2b450cf097a0dd2b393ae220b5ab66d1c31e6d
mortonprod <mortonprod@gmail.com>
2016-11-16 01:02:34 +0000

    Add user actions

---
+++ src/client/actions/userAction.ts
@@ -0,0 +1,15 @@
+﻿import actionNames from "./actionNames";
+
+export function addName(name: string) {
+    return {
+        type: actionNames().addName,
+        name
+    }
+}
+
+export function addEmail(email: string) {
+    return {
+        type: actionNames().addEmail,
+        email
+    }
+}
