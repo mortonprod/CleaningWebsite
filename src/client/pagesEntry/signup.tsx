b7d0552497f9c3a8f3db60d1a860bf40d7256bc9
mortonprod <mortonprod@gmail.com>
2016-11-16 01:01:59 +0000

    Add entry files

---
+++ src/client/pagesEntry/signup.tsx
@@ -0,0 +1,23 @@
+﻿import * as $ from "jquery";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import "../sass/site.scss";
+import * as React from 'react';
+import { Component } from 'react';
+import * as reactDOM from "react-dom";
+import thunkMiddleware from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import store  from '../reducers/index';
+import Signup from "../containers/signup";
+
+$(document).ready(function () {
+    reactDOM.render(
+        <Provider store={store()} >
+            <Signup/>
+        </Provider>,
+        document.getElementById("react-signup")
+    );
+});
+
+
