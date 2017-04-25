0b51f31c9baca5e03074ae59aa5cb8adfed777b9
mortonprod <mortonprod@gmail.com>
2017-04-25 17:26:49 +0100

    Update client files
    Delete old locations of componnts
    Remove old polyfill
    Update tsconfig to compile the typescript files

--- src/client/components/datetime/index.tsx
+++ src/client/components/datetime/index.tsx
@@ -1,158 +1,22 @@
-﻿/// <reference path="../../../../typings/index.d.ts" />
-import * as React from 'react';
-import * as Datetime from "react-datetime";
-let Spinner: any = null;
-if (typeof window === "undefined") {
-    console.log("On server do not include component with css")
-} else {
-    let Spinner = require('react-spinkit');
+﻿import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { DateTime } from "./comp/index";
+import actions from './actions';
+import reducer from './reducer';
+
+const mapStateToProps = (state: any, ownProps: any) => {
+    return {
+        sending: state.datetimeReducer.sendingBooking,
+        sentSuccess: state.datetimeReducer.sentBooking,
+        inValidDatesAndTimes: state.datetimeReducer.allDatesAndTimes,
+        yourDatesAndTimes: state.datetimeReducer.yourDatesAndTimes
+    }
 }
 
-
-//var Spinner = require('react-spinkit');
-interface state {
-    currentMoment: moment.Moment,
-    listOfMoments: Array<moment.Moment>
-}
-interface props {
-    sending: Boolean,
-    inValidDatesAndTimes: Array<moment.Moment>,
-    yourDatesAndTimes: Array<moment.Moment>,
-    sentSuccess: Boolean,
-    sendBooking: any
-}
-/**
- * @class
- * Implementation of DateTime component
- */
-export class DateTime extends React.Component<props, state> {
-    constructor(props: any) {
-        super(props);
-    }
-    noTimes: Array<string> = [];
-    //Steps of two hours 9am min 3pm max.
-    con = { hours: { min: 9, max: 15, step: 2 } }
-    /**
-     * @function
-     * Call when the date changes.
-     * Check this date for all the times we can't use so we can render it.
-     * @param moment
-     */
-    dateChange(moment: moment.Moment) {
-        this.noTimes = [];
-        console.log(moment.format('LLLL'));
-        this.setState({
-            currentMoment: moment,
-            listOfMoments: this.state.listOfMoments
-        })
-        for (let i = 0; this.props.inValidDatesAndTimes.length; i++) {
-            let momentStored: moment.Moment = this.props.inValidDatesAndTimes[i];
-            if (momentStored.date() === moment.date() && momentStored.month() === moment.month() && momentStored.year() === moment.year()) {
-                this.noTimes.push(moment.format("hh:mm:ss a"));
-            }
-        }
-    }
-    /**
-     * Check the days which we can't use.
-     * Return true on the days we want to keep
-     * @param current
-     */
-    valid(current: moment.Moment) {
-        if (typeof this.props.inValidDatesAndTimes !== "undefined" && this.props.inValidDatesAndTimes !== null ) {
-            for (let i = 0; this.props.inValidDatesAndTimes.length; i++) {
-                let momentStored: moment.Moment = this.props.inValidDatesAndTimes[i];
-                if (current.day === momentStored.day && current.month !== momentStored.month && current.year !== momentStored.year) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-    componentWillMount() {
-        this.setState({
-            currentMoment: null,
-            listOfMoments: []
-        })
-    }
-    submitHandler() {
-        this.props.sendBooking(this.state.listOfMoments);
-    }
-    /**
-     * @function
-     * Call when we want to add a new date to the list.
-     */
-    addHandler() {
-        let list: any
-        if (this.state.currentMoment !== null) {
-            list = this.state.listOfMoments.concat(this.state.currentMoment);
-        } else {
-            list = this.state.listOfMoments;
-        }
-        this.setState({
-            currentMoment: null,
-            listOfMoments: list
-        })
-    }
-    render() {
-        let comp: any;
-        if (this.props.sending) {
-            comp = (
-                <Spinner spinnerName='double-bounce'/>
-            )
-        } else {
-            let curDis: any
-            if (this.state.currentMoment !== null) {
-                curDis = (
-                    <div>
-                        <h5> {this.state.currentMoment.format('LLLL')} </h5>
-                    </div>
-                )
-            }
-            var list: Array<any> = [];
-            for (let i = 0; i < this.state.listOfMoments.length; i++) {
-                console.log(this.state.listOfMoments[i]);
-                list.push(
-                    <div>
-                        <h5> {this.state.listOfMoments[i].format('LLLL')} </h5>
-                    </div>
-                );
-            }
-            var noTimesComp: Array<any> = [];
-            for (let i = 0; i < this.noTimes.length; i++) {
-                console.log(this.noTimes[i]);
-                noTimesComp.push(
-                    <div>
-                        <h5> {this.noTimes[i]} </h5>
-                    </div>
-                );
-            }
-            comp = (
-                <div className="row height text-center">
-                    <div className="col-lg-4">
-                        <h3><span className="glyphicon glyphicon-ok"></span> Add</h3>
-                        {curDis}
-                        <button onClick={this.addHandler.bind(this)} className='btn btn-lg btn-primary btn-block'> Add </button>
-                        <Datetime onChange={this.dateChange.bind(this)} isValidDate={this.valid.bind(this)} timeConstraints={this.con}/>
-                    </div>
-                    <div className="col-lg-3">
-                        <h3><span className="glyphicon glyphicon-remove-sign"></span>Not available</h3>
-                        {noTimesComp}
-                    </div>
-                    <div className="col-lg-3">
-                        <h3><span className="glyphicon glyphicon-list"></span> Check</h3>
-                        {list}
-                    </div>
-                    <div className="col-lg-2">
-                        <h3><span className="glyphicon glyphicon-thumbs-up"></span> Submit</h3>
-                        <button onClick={this.submitHandler.bind(this)} className='btn btn-lg btn-primary btn-block'> Submit </button>
-                    </div>
-                </div>
-            )
-        }
-        return (
-            <div>
-                {comp}
-            </div>
-        );
+const mapDispatchToProps = (dispatch: any, ownProps: any) => {
+    return {
+        sendBooking: bindActionCreators(actions().sendBooking, dispatch)
     }
 }
+let DateTimeConnect = connect(mapDispatchToProps, mapDispatchToProps)(DateTime);
+export { DateTimeConnect, reducer };
--- src/client/components/reviews/index.tsx
+++ src/client/components/reviews/index.tsx
@@ -1,168 +1,19 @@
-﻿///File contact - the link between the contact actions and presentation.
-import * as React from 'react';
-import ReviewsPresentation from './reviewsPresentation';
-interface review { name: string, stars: number, message: string }
+﻿import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Reviews } from "./comp/index";
+import actions from './actions';
+import reducer from './reducer';
 
-interface props {
-    submit: any,
-    reviews: Array<review>,
-    showNumber?: number
-}
-interface state {
-    titleStyle: string,
-    initialStars: number,
-    reviewsToPass: Array<review>
-
-}
-export class Reviews extends React.Component<props, state> {
-    constructor(props: any) {
-        super(props);
-    }
-    initRan = false;
-    static defaultProps = {
-        reviews: [
-            { name: "Alex1", stars: 4, message: "Something like a message" },
-            { name: "Alex2", stars: 4, message: "Something like a message" },
-            { name: "Alex3", stars: 4, message: "Something like a message" },
-            { name: "Alex4", stars: 4, message: "Something like a message" },
-            { name: "Alex5", stars: 4, message: "Something like a message" },
-            { name: "Alex6", stars: 4, message: "Something like a message" },
-            { name: "Alex7", stars: 4, message: "Something like a message" },
-            { name: "Alex8", stars: 4, message: "Something like a message" },
-            { name: "Alex9", stars: 4, message: "Something like a message" },
-            { name: "Alex10", stars: 4, message: "Something like a message" },
-            { name: "Alex11", stars: 4, message: "Something like a message" },
-            { name: "Alex12", stars: 4, message: "Something like a message" }
-
-        ],
-        submit: null as any,
-        showNumber: 5
-    }
-    getShowNum() {
-        return this.state.reviewsToPass.length;
-    }
-    /**
-     * @function
-     * If we have more reviews then update the state to show all the review we where showing before.
-     * If we have less review now then show only these
-     * @param nextProps
-     */
-    componentWillReceiveProps(nextProps: props) {
-        let tempNum: number;
-        if (this.getShowNum() > nextProps.reviews.length) {
-            tempNum = nextProps.reviews.length;
-        } else {
-            tempNum = this.getShowNum();
-        }
-        let temp = [] as any
-        for (let i = 0; i < tempNum; i++) {
-            temp.push(nextProps.reviews[i]);
-        }
-        this.setState({
-            titleStyle: "",
-            initialStars: 0,
-            reviewsToPass: temp
-        })
-    }
-    componentWillMount() {
-        let temp = [] as any
-        let tempNum: number;
-        if (this.props.showNumber > this.props.reviews.length) {
-            tempNum = this.props.reviews.length;
-        } else {
-            tempNum = this.props.showNumber;
-        }
-        for (let i = 0; i < tempNum; i++) {
-            temp.push(this.props.reviews[i]);
-        }
-        this.setState({
-            titleStyle: "",
-            initialStars: 0,
-            reviewsToPass: temp
-        })
-    }
-    /**
-     * Call this on hover over the component.
-     * Makes the component a bit more lively.
-     */
-    init() {
-        if (!this.initRan) {
-            this.initRan = true;
-            this.setState({
-                titleStyle: "animated bounce",
-                initialStars: this.state.initialStars,
-                reviewsToPass: this.state.reviewsToPass
-            });
-            for (let i = 0; i < 7; i++) {
-                (function (i: number) {
-                    setTimeout(function () {
-                        let temp: number;
-                        if (i === 6) {
-                            temp = 0
-                        } else {
-                            temp = i
-                        }
-                        this.setState({
-                            titleStyle: this.state.titleStyle,
-                            initialStars: temp,
-                            reviewsToPass: this.state.reviewsToPass
-                        })
-                        console.log("i: " + i)
-                    }.bind(this), 1000 * i)
-                }.bind(this))(i)
-            }
-        }
-    }
-    showHandler(more: Boolean) {
-        let newTotal: number;
-        if (more) {
-            newTotal =  this.getShowNum() + this.props.showNumber;
-        } else {
-            newTotal = this.getShowNum() - this.props.showNumber;
-        }
-        let tempNum: number;
-        if (newTotal > this.props.reviews.length) {
-            tempNum = this.props.reviews.length;
-        } else {
-            tempNum = newTotal;
-        }
-        let temp = [] as Array<review>;
-        for (let i = 0; i < tempNum; i++) {
-            temp.push(this.props.reviews[i]);
-        }
-        this.setState({
-            titleStyle: this.state.titleStyle,
-            initialStars: this.state.initialStars,
-            reviewsToPass: temp
-        })
-    }
-    /**
-     * @function
-     * Add review to list then call dispatch to store.   
-     * @param rev
-     */
-    submit(rev: review) {
-        //Remove below when we have store working
-//        this.props.submit(rev)
-        let temp = this.state.reviewsToPass;
-        temp.push(rev);
-        this.setState({
-            titleStyle: this.state.titleStyle,
-            initialStars: this.state.initialStars,
-            reviewsToPass: temp
-        })
-    }
-    render() {
-        return (
-            <div onMouseOver={() => { this.init() } }>
-                <ReviewsPresentation
-                    reviews={this.state.reviewsToPass}
-                    submit={(rev: review) => { this.submit(rev) } }
-                    titleStyle={this.state.titleStyle}
-                    initialStars={this.state.initialStars}
-                    showHandler = {(moreBool: Boolean) => { this.showHandler(moreBool) } }
-                    />
-            </div>
-        );
+const mapStateToProps = (state: any, ownProps: any) => {
+    return {
+        reviews: state.reviewsReducer.reviews
     }
 }
+const mapDispatchToProps = (dispatch: any, ownProps: any) => {
+    return {
+        submit: bindActionCreators(actions().sendingReview, dispatch)
+    }
+}
+let ReviewConnect = connect(mapDispatchToProps, mapDispatchToProps)(Reviews);
+
+export {ReviewConnect, reducer}
