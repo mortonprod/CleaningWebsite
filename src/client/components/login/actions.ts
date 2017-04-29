db8c66e5ea2682a67ccc0b6623fb9431a457e560
mortonprod <mortonprod@gmail.com>
2017-04-29 02:05:39 +0100

    Add new login
    Create login presentation component
    Create stateful component
    Create reducer
    Create actions

---
+++ src/client/components/login/actions.ts
@@ -0,0 +1,62 @@
+﻿import * as $ from "jquery";
+
+/**
+ * @function
+ * Actions(functions) for login
+ * Contains all functionality of component.
+ * Action attached to component through "connect" function.
+ * Action can:
+ *  1) Be called to run some function and then pass to function which updates store
+ *  2) Update the store directly(The small funcitons which return action name and updated parameter).
+ */
+export default function actions() {
+    function names() {
+        return {
+            setSending: "SET_SENDING",
+            setError: "SET_ERROR"
+        }
+    }
+    function sendLoginDetailsToServer(email: string, password: string) {
+        return (dispatch: any) => {
+            console.log("Send to server");
+            dispatch(sending(true));
+            $.ajax({
+                type: 'POST',
+                url: '/login',
+                data: { email, password }
+            }).done(function (data) {
+                console.log("Sent to server pass data:" + JSON.stringify(data));
+                dispatch(sending(false));
+                if (data.error) {
+                    addError(data.error);
+                } else {
+                }
+            }).fail(function (a, b, c, d) {
+                console.log("Sent to server fail");
+                dispatch(sending(false));
+                addError("Error sending to server");
+            });
+        }
+
+    }
+
+    function sending(bool: Boolean) {
+        return {
+            type: names().setSending,
+            sending: bool
+        }
+    }
+
+    function addError(error: string = null) {
+        return {
+            type: names().setError,
+            errorMessage: error
+        }
+    }
+    return {
+        names,
+        sendLoginDetailsToServer,
+        sending,
+        addError
+    }
+}
---
+++ src/client/components/login/index.ts
@@ -0,0 +1,19 @@
+﻿import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Login } from "./comp/index";
+import actions from './actions';
+import reducer from './reducer';
+
+
+const mapStateToProps = (state: any, ownProps: any) => {
+    return {
+        sending: state.loginReducer.sendingLogin
+    }
+}
+const mapDispatchToProps = (dispatch: any, ownProps: any) => {
+    return {
+        sendLoginDetailsToServer: bindActionCreators(actions().sendLoginDetailsToServer, dispatch)
+    }
+}
+let LoginConnect = connect(mapDispatchToProps, mapDispatchToProps)(Login);
+export {LoginConnect, reducer}
